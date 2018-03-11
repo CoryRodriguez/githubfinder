3513f0bc588b086e035161e4f303a44999e48f5e
Cory Rodriguez <mrcoryrodriguez@gmail.com>
2018-03-11 15:59:32 -0400

    edited JS template literals for UI display

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,28 @@
 // Init GitHub
-const github = new Github;
+const github = new Github();
+
+// Init UI
+const ui = new UI();
 
 // Search Input
-const searchUser = document.getElementById('searchUser');
+const searchUser = document.getElementById("searchUser");
 
 // Search Inputer Event Listener
-searchUser.addEventListener('keyup', (e) => {
+searchUser.addEventListener("keyup", e => {
   // Get input text
   const userText = e.target.value;
 
-  if (userText !== '') {
+  if (userText !== "") {
     // Make HTTP call
-    github.getUser(userText)
-      .then(data => {
-        if (data.profile.message === 'Not Found') {
-          // Show alert
-
-        } else {
-          // Show profile
-
-        }
-      })
+    github.getUser(userText).then(data => {
+      if (data.profile.message === "Not Found") {
+        // Show alert
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+      }
+    });
   } else {
     // Clear Profile
-    
   }
 });
