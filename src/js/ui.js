b1a4f47ed8a36b51fe284f1cc0dff7a24439879c
Cory Rodriguez <mrcoryrodriguez@gmail.com>
2018-03-15 13:10:40 -0400

    fixed centering based on screen size

--- src/js/ui.js
+++ src/js/ui.js
@@ -46,7 +46,7 @@ class UI {
     repos.forEach(repo => {
       output += `
         <div class="card card-body mb-2">
-          <div class="row ">
+          <div class="row">
             <div class="col-md-6 mb-2 mb-md-0">
               <a href="${repo.html_url}" target="_blank">${repo.name}</a>
             </div>
